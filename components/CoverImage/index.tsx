db08ea64bcf2f5b9cb3b5eaad4c29d5ea0ff426c
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-09-03 15:10:38 +0530

    Alt added

--- components/CoverImage/index.tsx
+++ components/CoverImage/index.tsx
@@ -9,6 +9,7 @@ const CoverImage = () => {
         src="/images/pic1.jpg"
         width={4000}
         height={1600}
+        alt="pic1"
       />
       <div className={styles.logoList}>
         <Image
@@ -16,54 +17,63 @@ const CoverImage = () => {
           src="/images/javascript.gif"
           width={50}
           height={50}
+          alt="javascript"
         />
         <Image
           className={styles.image}
           src="/images/ruby.png"
           width={50}
           height={50}
+          alt="ruby"
         />
         <Image
           className={styles.image}
           src="/images/c++.png"
           width={50}
           height={50}
+          alt="c++"
         />
         <Image
           className={styles.image}
           src="/images/github.gif"
           width={50}
           height={50}
+          alt="github"
         />
         <Image
           className={styles.image}
           src="/images/svelte.png"
           width={50}
           height={50}
+          alt="svelte"
         />
         <Image
           className={styles.image}
           src="/images/python.gif"
           width={50}
           height={50}
+          alt="python"
         />
         <Image
           className={styles.image}
           src="/images/mongo.png"
           width={50}
           height={50}
+          alt="mongo"
         />
         <Image
           className={styles.image}
           src="/images/postgres.png"
           width={50}
           height={50}
+          alt="postgres"
         />
         <Image
           className={styles.image}
           src="/images/linux.gif"
           width={50}
           height={50}
+          alt="linux"
         />
       </div>
     </div>
--- components/LogoImages/index.tsx
+++ components/LogoImages/index.tsx
@@ -11,42 +11,49 @@ const LogoImages = () => {
           src="/images/fog.gif"
           width={50}
           height={50}
+          alt="fog"
         />
         <Image
           className={styles.image}
           src="/images/haze.gif"
           width={50}
           height={50}
+          alt="haze"
         />
         <Image
           className={styles.image}
           src="/images/light-snow.gif"
           width={50}
           height={50}
+          alt="light-snow"
         />
         <Image
           className={styles.image}
           src="/images/rain-cloud.gif"
           width={50}
           height={50}
+          alt="rain-cloud"
         />
         <Image
           className={styles.image}
           src="/images/storm.gif"
           width={50}
           height={50}
+          alt="storm"
         />
         <Image
           className={styles.image}
           src="/images/rainbow.gif"
           width={50}
           height={50}
+          alt="rainbow"
         />
         <Image
           className={styles.image}
           src="/images/rainy-snowy.gif"
           width={50}
           height={50}
+          alt="rainy-snowy"
         />
       </div>
     </div>
